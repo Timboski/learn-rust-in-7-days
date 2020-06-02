742c34cb78e016781db0a412ed7c69f9acfe8ce2
Timbo <timboboyle@yahoo.co.uk>
2020-06-02 21:38:55 +0100

    Copy parser from solution

--- assignment-2/src/lib.rs
+++ assignment-2/src/lib.rs
@@ -1,7 +1,8 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//! A library for handling money in a typesafe way
+
+pub mod parse;
+//pub mod printer;
+
+//pub mod currencies;
+//pub mod exchange;
+
