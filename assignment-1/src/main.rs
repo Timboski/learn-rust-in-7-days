88dc8226a7e37280e48c70134fa8302b6e60f957
Tim Boyle <timboboyle@yahoo.co.uk>
2020-06-02 12:23:48 +0100

    Say Hello to everyone.

--- assignment-1/src/main.rs
+++ assignment-1/src/main.rs
@@ -1,3 +1,10 @@
+// Assignment 1 : Say Hello to everyone in the program argument
+// list whose name begins with the letter W.
+
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+    for name in args() {
+        println!("Hello {}", name);
+    }    
 }
