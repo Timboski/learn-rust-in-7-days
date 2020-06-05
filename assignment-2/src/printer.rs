a76ed558aad709dd4b74d991dca2af25cf93ec3d
Tim Boyle <timboboyle@yahoo.co.uk>
2020-06-05 11:20:51 +0100

    Handle positive and negative

--- assignment-2/src/printer.rs
+++ assignment-2/src/printer.rs
@@ -3,11 +3,22 @@
 ///
 /// ```
 /// use money_typesafe::printer::display_curr;
+/// assert_eq!(&display_curr(5,'£',2),"£0.05");
 /// assert_eq!(&display_curr(-3,'£',2),"-£0.03");
 // assert_eq!(&display_curr(-3456,'£',2),"-£34.56");
 // assert_eq!(&display_curr(3,'£',2),"£0.03");
 // assert_eq!(&display_curr(3456,'£',2),"£34.56");
 /// ```
 pub fn display_curr(mut v:i32,sym:char,dp:usize)->String{
-    "-£0.03".to_string()
+    let mut prefix = String::new();
+    let mut value = (v as f32) / 100.0;
+
+    if value < 0.0 {
+        prefix.push('-');
+        value = -value;
+    }
+
+    prefix.push(sym);
+ 
+    format!("{}{}",prefix,value)
 }
