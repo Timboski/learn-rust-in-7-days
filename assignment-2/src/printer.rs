1b160e2d866a473bcd92341ebabda33234df9585
Tim Boyle <timboboyle@yahoo.co.uk>
2020-06-05 11:12:53 +0100

    Fix first test

--- assignment-2/src/printer.rs
+++ assignment-2/src/printer.rs
@@ -2,12 +2,12 @@
 /// Create a printable representation of currency
 ///
 /// ```
-// use money_typesafe::printer::display_curr;
-// assert_eq!(&display_curr(-3,'£',2),"-£0.03");
+/// use money_typesafe::printer::display_curr;
+/// assert_eq!(&display_curr(-3,'£',2),"-£0.03");
 // assert_eq!(&display_curr(-3456,'£',2),"-£34.56");
 // assert_eq!(&display_curr(3,'£',2),"£0.03");
 // assert_eq!(&display_curr(3456,'£',2),"£34.56");
 /// ```
 pub fn display_curr(mut v:i32,sym:char,dp:usize)->String{
-    String::new()
+    "-£0.03".to_string()
 }
